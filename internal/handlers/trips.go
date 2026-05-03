6a0905694f78999d4b16698944d3db131c93b9c8
35-1_Nursultan_Myrzakimov_hw_6 <nursultanmyrzakimov077@gmail.com>
2026-05-04 01:50:39 +0600

    POST API trips

--- internal/handlers/trips.go
+++ internal/handlers/trips.go
@@ -33,3 +33,34 @@ func GetTrips(c *gin.Context) {
 
 	c.JSON(http.StatusOK, trips)
 }
+
+func CreateTrip(c *gin.Context) {
+	var trip models.Trip
+
+	// читаем JSON
+	if err := c.ShouldBindJSON(&trip); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// вставка в БД
+	query := `
+		INSERT INTO trips (title, start_date, end_date)
+		VALUES ($1, $2, $3)
+		RETURNING id
+	`
+
+	err := database.DB.QueryRow(
+		query,
+		trip.Title,
+		trip.StartDate,
+		trip.EndDate,
+	).Scan(&trip.ID)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, trip)
+}

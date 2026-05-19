76e85f6800db3489315dd257e97b12e92e4d2ac4
35-1_Nursultan_Myrzakimov_hw_6 <nursultanmyrzakimov077@gmail.com>
2026-05-20 01:03:53 +0600

    API events

---
+++ internal/handlers/events.go
@@ -0,0 +1,94 @@
+package handlers
+
+import (
+	"net/http"
+
+	"wayfarer/internal/database"
+	"wayfarer/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GET /api/events
+func GetEvents(c *gin.Context) {
+	rows, err := database.DB.Query(`
+		SELECT id, day_id, title, start_time, location_name, cost, description 
+		FROM events
+	`)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	events := []models.Event{}
+
+	for rows.Next() {
+		var e models.Event
+
+		rows.Scan(
+			&e.ID,
+			&e.DayID,
+			&e.Title,
+			&e.StartTime,
+			&e.LocationName,
+			&e.Cost,
+			&e.Description,
+		)
+
+		events = append(events, e)
+	}
+
+	c.JSON(http.StatusOK, events)
+}
+
+// POST /api/events
+func CreateEvent(c *gin.Context) {
+	var e models.Event
+
+	if err := c.ShouldBindJSON(&e); err != nil {
+		c.JSON(400, gin.H{"error": "invalid data"})
+		return
+	}
+
+	_, err := database.DB.Exec(`
+		INSERT INTO events(day_id, title, start_time, location_name, cost, description)
+		VALUES (?, ?, ?, ?, ?, ?)
+	`,
+		e.DayID,
+		e.Title,
+		e.StartTime,
+		e.LocationName,
+		e.Cost,
+		e.Description,
+	)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "event created",
+	})
+}
+
+// DELETE /api/events/:id
+func DeleteEvent(c *gin.Context) {
+	id := c.Param("id")
+
+	_, err := database.DB.Exec(
+		"DELETE FROM events WHERE id=?",
+		id,
+	)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "event deleted",
+	})
+}
